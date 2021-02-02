3a7c73fd25a785db96b4fda37a69fad253418daa
MARX CARAM ROSSI <987270925@itaud.des.ihf>
2021-02-02 15:01:45 -0300

    atualizando site

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="author" content="">
     <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
 
-    <title>Dashboard Template for Bootstrap</title>
+    <title>O mundo precisa saber quem é Marx, e não estamos falando do Karl</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/dashboard/">
 
