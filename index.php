2a1419f8334bbc51b9e64d68050b08da60f66975
MARX CARAM ROSSI <987270925@itaud.des.ihf>
2021-02-02 13:59:20 -0300

    atualizando site

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 teste php
 \n
 teste 2, me dê acaras discos
+
+<div style="display:flex; height: 400px">
+    <div style="background-color: blue">
+    </div>
+    <div style="background-color: yellow">
+    Esta é a cor amarelo
+</div>
+</div>
+
